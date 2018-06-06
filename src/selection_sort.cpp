0e56dd9114ef986953f1295210d7a3d8c640a87d
minh <minh0722@yahoo.com>
2018-06-06 07:35:49 +0000

    add comment

--- src/selection_sort.cpp
+++ src/selection_sort.cpp
@@ -1,7 +1,17 @@
 #include "sorts.h"
 #include "util.h"
 
-
+/*
+* Not stable sort
+* It is not stable since the smallest element is swapped to the front and it can change the order
+* Example:
+* 4 2 4 1
+* The first 4 will be swapped with 1 and the order of the 4's are reverted
+*
+*
+* It can be made to stable if we find the smallest element, remove it and push it to the front.
+* Then linked list should be used since insert is faster
+*/
 
 namespace algorithm
 {
