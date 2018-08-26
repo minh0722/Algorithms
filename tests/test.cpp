26ab94bc2fee346f7d0b01493dc0e95928c7bb8b
Minh Nguyen <minh0722@yahoo.com>
2018-08-27 01:59:59 +0300

    added test file. Not used yet

---
+++ tests/test.cpp
@@ -0,0 +1,30 @@
+#include "sorts.h"
+#include "gmock\gmock.h"
+#include "gtest\gtest.h"
+#include "util.h"
+#include <vector>
+#include <random>
+using testing::Bool;
+
+int generator()
+{
+	static std::mt19937 randomGenerator;
+	static std::uniform_int_distribution<int> distribution(1, 100000);
+
+	return distribution(randomGenerator);
+}
+
+TEST(InsertionSort, SortIsCorrect)
+{
+	std::vector<int> v(100);
+	std::generate(v.begin(), v.end(), generator);
+	algorithm::sort::insertionSort(v);
+
+	EXPECT_TRUE(util::isSorted(v));
+}
+
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
