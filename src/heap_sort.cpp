8b521effddd2a9181191237dbfa283ce2a828908
Minh Nguyen <minh0722@yahoo.com>
2024-04-08 16:31:08 +0300

    add some comments to heap sort

--- src/heap_sort.cpp
+++ src/heap_sort.cpp
@@ -2,6 +2,11 @@
 #include "util.h"
 #include <cstddef>
 
+/*
+* Not stable sort
+* nlog(n) worst case complexity
+*/
+
 namespace algorithm
 {
     namespace sort
@@ -20,12 +25,17 @@ namespace algorithm
 
             while (left < size)
             {
+                // get the max child of the 2 children
                 maxIdx = v[left] > v[right] ? left : right;
 
+                // if the parent is already bigger than max child, then no need to proceed
                 if (v[index] >= v[maxIdx])
                     break;
 
+                // swap the parent with the max child
                 util::swap(v[index], v[maxIdx]);
+
+                // change the parent index to the max child that we just swapped and get its children indexes
                 index = maxIdx;
                 left = index * 2 + 1;
                 right = index * 2 + 2 < size ? index * 2 + 2 : left;
@@ -41,9 +51,14 @@ namespace algorithm
                 heapifyDown(v, i, size);
             }
 
+            // start the normalizing process that converts the heap into sorted array
             for (size_t i = size - 1; i > 0; --i)
             {
+                // swap the top element with the last element
                 util::swap(v[i], v[0]);
+
+                // then heapify down the top element up until the last element that we just swap
+                // this ensure that the last element will be the biggest element in the array
                 heapifyDown(v, 0, i);
             }
         }
